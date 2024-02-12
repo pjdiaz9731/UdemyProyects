369eb22ad4d8d9509ec2911d3eaf657c5b49908d
Dcb11CR <dcamposborbon@gmail.com>
2024-02-11 23:14:49 -0600

    Ejercicio 3 Java

--- Ejercicios Java Diego/Ejercicio4_PedirNumero/src/pedir_numero/Pedir_Numero.java
+++ Ejercicios Java Diego/Ejercicio4_PedirNumero/src/pedir_numero/Pedir_Numero.java
@@ -1,15 +1,23 @@
-
-package ejercicio4_pedirnumeros;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pedir_numero;
 
 import java.util.Scanner;
 
+/**
+ *
+ * @author Diego
+ */
+public class Pedir_Numero {
 
-public class Ejercicio4_PedirNumeros {
-
-
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        
-        /* Este programa pedirá una indefinida cantidad de números, 
+       
+           /* Este programa pedirá una indefinida cantidad de números, 
         y en el momento en que el usuario teclee un cero, 
         dejará de pedir más números. Al final mostrará la suma de todos los números introducidos. */
         
@@ -18,19 +26,9 @@ public class Ejercicio4_PedirNumeros {
         
         do {            
             System.out.println("Introduce un número");
-            
+             
             
         } while (num != 0);
-        
-
-
-
-
-
-
-
-
-
 
     }
     
