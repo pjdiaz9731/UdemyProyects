036387b70c0b308ffd5ee048bbecffc506b39922
Dcb11CR <dcamposborbon@gmail.com>
2024-02-11 23:05:07 -0600

    Ejercicio 4 de Java agregado

---
+++ Ejercicios Java Diego/Ejercicio4_PedirNumeros/src/ejercicio4_pedirnumeros/Ejercicio4_PedirNumeros.java
@@ -0,0 +1,37 @@
+
+package ejercicio4_pedirnumeros;
+
+import java.util.Scanner;
+
+
+public class Ejercicio4_PedirNumeros {
+
+
+    public static void main(String[] args) {
+        
+        /* Este programa pedirá una indefinida cantidad de números, 
+        y en el momento en que el usuario teclee un cero, 
+        dejará de pedir más números. Al final mostrará la suma de todos los números introducidos. */
+        
+        int num = 0, suma = 0;
+        Scanner sc = new Scanner(System.in);
+        
+        do {            
+            System.out.println("Introduce un número");
+            
+            
+        } while (num != 0);
+        
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
