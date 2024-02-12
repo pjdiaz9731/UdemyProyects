bf2273319d56dc82563a80a2e8e2b89a7a67dbe9
Dcb11CR <dcamposborbon@gmail.com>
2024-02-11 21:43:32 -0600

    Prueba1

---
+++ Holamundo/src/holamundo/Holamundo.java
@@ -0,0 +1,10 @@
+package holamundo;
+
+public class Holamundo {
+
+    /* Programa holamundo */
+    public static void main(String[] args) {
+// Este programa solo muestra un saludo por la pantalla
+        System.out.println("Hola, mundo!");
+    }
+}
