f6740b08da5e152ccab8d146703dcf862dc32bba
Pablo Díaz <pdiaz9731@gmail.com>
2024-02-16 21:54:49 -0600

    Separar alumnos respecto a sus notas Ejer8

---
+++ Ejercicios Java Pablo/Ejercicio9/src/main/java/com/mycompany/ejercicio9/Ejercicio9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio9;
+
+/**
+ *
+ * @author pjdiaz
+ */
+public class Ejercicio9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
