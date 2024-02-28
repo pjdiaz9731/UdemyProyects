9d8e64b3e6a10e41da0a1eaeb12b01fe623a4d58
Diego Campos <dcamposborbon@gmail.com>
2024-02-28 13:11:11 -0600

    carteta prueba1 quicksort
    carpeta prueba2 Colas
    
    carpeta prueba3 Tarea1_Colas

---
+++ Ejercicios Java Diego/prueba2/Colas/Colas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colas
+{
+    internal class Colas
+    {
+        class Nodo 
+        {
+            public int info;
+            public Nodo sig;
+        }
+        private Nodo raiz, fondo;
+        public Colas() 
+        {
+            raiz = null;
+            fondo = null;
+        }
+        public bool Vacia() 
+        {
+            if (raiz == null)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
