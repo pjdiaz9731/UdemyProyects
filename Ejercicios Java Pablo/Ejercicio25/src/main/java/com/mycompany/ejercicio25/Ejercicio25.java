206c926ceb224636d34e211006456bb1fadbd60d
Pablo Díaz <pdiaz9731@gmail.com>
2024-03-06 23:42:28 -0600

    LLenar matriz con numeros positivos

---
+++ Ejercicios Java Pablo/Ejercicio25/src/main/java/com/mycompany/ejercicio25/Ejercicio25.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio25;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pjdiaz
+ */
+public class Ejercicio25 {
+
+    public static void main(String[] args) {
+       int matriz [][]= new int[3][3];
+       Scanner sc= new Scanner(System.in);
+        System.out.println("***Ingresa los numeros de la matri***");
+        for (int i = 0; i <matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.println("Ingresa numero: ");
+                int num=sc.nextInt();
+                if (num>=0){
+                matriz[i][j]=num;
+                j++;
+                }
+                else {
+                        System.out.println("El numero es negativo" + "No se puede ingresar en la matriz");
+                        }
+                
+                }
+                
+            }
+        System.out.println("Mostrar matriz: ");
+        for (int i = 0; i < matriz.length; i++) {
+            
+        }
+        System.out.println("\n");
+        }
+    
+    
+    }
+
